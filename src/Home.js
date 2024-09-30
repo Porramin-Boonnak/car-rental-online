9df496ce9d914b2e3f5f3aa376e6b01b0aa51a16
Porramin <mosphonez3@gmail.com.com>
2024-09-30 16:07:54 +0700

    create_home_fristtake

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,16 @@
+import './css/Home.css'
 export default function home(){
     return (<>
-        <h1>Hello</h1>
+        <div>
+            <div id="tabbar"></div>
+            <div id="center">
+                <div id="pic">pic</div>
+                <input id="inputtop" type="text"/>
+                <br/>
+                <br/>
+                <input id="frist" type="text"/>
+                <input type="text"/>
+            </div>
+        </div>
     </>)
 }
