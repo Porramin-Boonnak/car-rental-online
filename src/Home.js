f4baf8be6a4a1598e53965968c472a1afc11afef
siriwimon <73596684+Alberta00@users.noreply.github.com>
2024-10-04 15:16:38 +0700

    Icon

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,26 @@
 import './css/Home.css'
-export default function home(){
+export default function home() {
     return (<>
+
         <div>
-            <div id="tabbar"></div>
-            <div id="center">
-            <div id = "homebackG"></div>
-                <div id="pic">pic</div>
-                <input id="inputtop" type="text"/>
-                
-                <br/>
-                <br/>
-                <input id="first" type="text"/>
-                <input type="text"/>
+            <div id="homebackG">
+
+                <div id="tabbar">
+                    <img src = "https://cdn-icons-png.flaticon.com/512/4254/4254068.png"/>
+                    <img id="profileicon" src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" width="45" height="47" alt="Profile Icon" />
+                </div>
+                <div id="center">
+                    <img id="logoicon" src="https://pngimg.com/uploads/Mclaren/Mclaren_PNG2.png" width="280" height="150" alt="McLaren Logo" />
+                    <div>
+                        <div id="pic">pic
+                        </div>
+                        <input id="inputtop" type="text" /></div>
+
+                    <br />
+                    <br />
+                    <input id="first" type="text" />
+                    <input type="text" />
+                </div>
             </div>
         </div>
     </>)
