117575dbf6986cb597c9d6f5d5e914c001ce1c7d
Porramin <mosphonez3@gmail.com.com>
2024-10-14 04:12:32 +0700

    list_update_filter

--- src/list.js
+++ src/list.js
@@ -7,10 +7,13 @@ const Item=(props)=>{
         id: {props.id}<br/>
         Name: {props.name}<br/>
         Price: {props.price}<br/>
+        location: {props.location}<br/>
     </div>)
 }
 export default function List(){
     const [products,setProducts] = useState([])
+    const [location,setlocation] = useState("empty");
+    const [types,settypes] = useState("empty");
     const url = "http://localhost:5000";
     useEffect(()=>{
         axios.get(url+'/api/car').then(response=>{
@@ -22,7 +25,13 @@ export default function List(){
 
         }
     },[])
-    const productlist=products.map(item=><Item {...item}/>)
+    var productlist=products.filter(item => (item.location === location || location === "empty") && (item.type === types || types === "empty")).map(item => <Item {...item} />);
+    const hendlechange=(event)=>{
+        setlocation(event.target.value);
+    }
+    const hendleclick=(event)=>{
+        settypes(event.target.value);
+    }
     return (<>
     <div id='main'>
         <label id='textstartlist'>Start</label>
@@ -35,12 +44,22 @@ export default function List(){
                 type="datetime-local"
                 id='endlist'
                 />
-        <input id='locationlist' type='text' placeholder='location rent car'/>
+            <select id="locationlist" onChange={hendlechange}>
+            <option value="empty">--Please choose a location--</option>
+            <option value="bangkok">Bangkok</option>
+            <option value="chiang Mai">Chiang Mai</option>
+            <option value="chonburi">Chonburi</option>
+            <option value="phuket">Phuket</option>
+            <option value="ubon Ratchathani">Ubon Ratchathani</option>
+            <option value="songkhla">Songkhla</option>
+        </select>
         <div id='sidebar'>
-            <label>รถ2ประตู</label>
-            <label>รถ4ประตู</label>
+            <button value="empty" onClick={hendleclick}>Reset</button>
+            <button value="3 door" onClick={hendleclick}>รถ3ประตู</button>
+            <button  value="4 door" onClick={hendleclick}>รถ4ประตู</button>
+            <button  value="5 door" onClick={hendleclick}>รถ5ประตู</button>
+            
         </div>
-        <input id='Start_search' type="button" value="Start search" />
         <div id='content'>
             {productlist}
         </div>
