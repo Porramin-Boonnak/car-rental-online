329bd867decd3a11e164df377527d9bde2f106e7
siriwimon <73596684+Alberta00@users.noreply.github.com>
2024-10-14 23:13:10 +0700

    ThxPage

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import Selectcar from "./selectcar"
 import List from "./list"
 import Payment from "./Payment"
+import Thx from "./Thx"
 export default function Router() {
     return (
       <BrowserRouter>
@@ -11,6 +12,7 @@ export default function Router() {
           <Route path="/selectcar" element={<Selectcar />}/>
           <Route path="/list" element={<List />}/>
           <Route path="/Payment" element={<Payment />}/>
+          <Route path="/Thx" element={<Thx />}/>
         </Routes>
       </BrowserRouter>
     );
---
+++ src/Thx.js
@@ -0,0 +1,18 @@
+import './css/Thx.css'
+import { useLocation } from 'react-router-dom';
+export default function Thx() {
+    return (<>
+    <div id='mainpageThx'>
+        <div id = "carThx">
+            <img id = "carpicThx"src = "https://images-ext-1.discordapp.net/external/Kxz70stGCEYR5Yl3NgzySXtdR4zZtfhOLF6ZNL9ccTI/https/imgcdn.zigwheels.co.th/large/gallery/color/11/3588/isuzu-mu-x-2024-color-485022.jpg?format=webp&width=993&height=662"/>
+        </div>
+        
+        <span id = "B">Success Booking</span>
+        <span id = "S">♥Have a nice trip♥</span>
+
+        <label id = "BookingThx">Home</label>
+
+
+    </div>
+    </>)
+}
