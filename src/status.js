538c0219d4e3efd602f3e8f3cdc8e7544c9edf82
siriwimon <73596684+Alberta00@users.noreply.github.com>
2024-10-19 19:25:27 +0700

    status-part2-Almost

--- src/status.js
+++ src/status.js
@@ -3,11 +3,25 @@ export default function status() {
     return (<>
         <div id='mainstaus'>
             <div id = "borderstatus">
-            <img id="picprostatus" src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" alt='car'/>
+            <img id="picprostatus" src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png"/>
             </div>
 
             <label id = "namestatus">name: </label>
             <label id = "realnamestatus">....</label>
+
+            <div id = "bigbox">
+            <img id="carselect" src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" alt='car'/>
+
+
+            </div>
+            <div id = "bigbox2">
+            
+            
+
+
+            </div>
+            <label id = "come">สถานที่รับ</label>
+            <label id = "out">สถานที่คืน</label>
         
             
 
