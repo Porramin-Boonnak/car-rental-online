9c5243ecc3b4fbf82377a637e50f9fe9953d9ac0
Porramin <mosphonez3@gmail.com.com>
2024-10-21 05:29:14 +0700

    login page can connect with mysql

--- src/login.js
+++ src/login.js
@@ -1,8 +1,28 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './css/login.css'
+import axios from 'axios';
 export default function Login() {
+    const url = "http://localhost:5000";
     const username = useRef(null);
     const password = useRef(null);
+    const navigate = useNavigate();
+    const Handleclick=()=>{
+        const data = {
+            username : username.current.value,
+            password : password.current.value
+        }
+        axios.post(url+'/api/customerlogin',data).then(response=>{
+            if(response.data==="Successfully")
+            {
+                console.log(response.data)
+                navigate('/list');
+            }
+        }).catch(error=>{
+            alert("Username and password do not match")
+        });
+        
+    }
     return (<>
         <div id='mainlogin'>
             <img id="logopiclogin" src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" alt='car'/>
@@ -10,9 +30,7 @@ export default function Login() {
                 <div id="username">
                     <label>USERNAME/EMAIL</label>
                     <input type="text" ref={username}/>
-
                 </div>
-
                 <div id="password">
                     <label>Password</label><br />
                     <input type="text" ref={password}/>
@@ -20,23 +38,8 @@ export default function Login() {
                 <div id="signup">
                     <label>Signup</label>
                 </div>
-                <div id="blogin">login</div>
+                <input id="blogin" type='submit' onClick={Handleclick} value='login'/>
             </div>
-
-
-
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
     </>)
 }
