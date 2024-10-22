0fc39d3c35f7c61c6193edd1a5915e107db6bb20
siriwimon <73596684+Alberta00@users.noreply.github.com>
2024-10-22 17:21:57 +0700

    StatusPage

--- src/status.js
+++ src/status.js
@@ -16,17 +16,22 @@ export default function status() {
             </div>
             <div id = "bigbox2">
             
-            
-
-
             </div>
-            <label id = "come">สถานที่รับ</label>
-            <label id = "out">สถานที่คืน</label>
-        
+            <label id = "come">สถานที่รับ:</label><br/>
+            <label id = "out">สถานที่คืน:</label><br/>
+            <label id = "comedate">label</label>
+            <label id = "outdate">label</label>
+            <div id = "comeoutdate"><input type="date" placeholder="MM/DD/YYYY" className='InputDate'></input></div>
+            <div id = "comeoutdate1"><input type="date" placeholder="MM/DD/YYYY" className='InputDate'></input></div>
+            <img id = "checkpic"src="https://cdn-icons-png.flaticon.com/512/1828/1828778.png" alt="Document Icon"/>
+            <div id = "rounded-rectangle"></div>
+            <img id = "checkpic1"src="https://cdn-icons-png.flaticon.com/512/1828/1828778.png" alt="Document Icon"/>
+            <div id = "rounded-rectangle1"></div>
+            <img id = "checkpic2"src="https://cdn-icons-png.flaticon.com/512/1828/1828778.png" alt="Document Icon"/>
+            <label id = "textlastlabel">label</label>
             
 
-
-
+            
         </div>
     </>)
 }
