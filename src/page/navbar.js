b0bf4324c9f304001f354ddc968eec99d1196551
Nokia3310Ww <comgamer15@gmail.com>
2024-11-06 03:48:14 +0700

    navbar

--- src/page/navbar.js
+++ src/page/navbar.js
@@ -1,6 +1,28 @@
 import Router from "./Router"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faRightToBracket, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+
+import '../css/navbar.css'
+
 export default function Nevbar(){
     return (<>
+        <div className="containernavbar">
+            <FontAwesomeIcon id="logohomeNav" icon={faHome} />
+            {/*<button id="grouploginNav">
+                <h1 id="textloginNav">LOGIN</h1>
+                <FontAwesomeIcon id="logologinNav" icon={faRightToBracket} />
+            </button>*/}
+            <button id="grouplogoutNav">
+                <h1 id="textlogoutNav">LOGOUT</h1>
+                <FontAwesomeIcon id="logologoutNav" icon={faRightFromBracket} />
+            </button>
+            <button id="user">
+                <label id="iduserNav">userid</label>
+                <FontAwesomeIcon id="userNav" icon={faUser} />
+            </button>
+        </div>
         <Router />
+        
     </>)
 }
